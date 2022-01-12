99bbd496852cb2dbc8c3ae4ad60a38da61bb7cc2
jleba <jleba@DESKTOP-MHEG86M>
2022-01-11 17:05:32 -0700

    Add project files.

---
+++ Mission_2/wwwroot/js/Calculate.js
@@ -0,0 +1,7 @@
+﻿$("#btnCalc").click(function () {
+    alert("Calculated Grade: " + ((parseInt($("#numAssignment").val()) * .55) +
+        (parseInt($("#numProject").val()) * .05) + (parseInt($("#numQuizzes").val()) * .1) + 
+        (parseInt($("#numExams").val()) * .2) + (parseInt($("#numINTEX").val()) * .1)).toFixed(2));
+
+    $("#hockey").fadeToggle("slow");
+})
